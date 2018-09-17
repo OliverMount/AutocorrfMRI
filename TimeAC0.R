48c556eead048f2cd573b74d041e1c26845e3464
OliverMount <oliverjmount@yahoo.com>
2018-09-17 11:13:37 +0900

    Unbiased autocorrelation estimator

---
+++ TimeAC0.R
@@ -0,0 +1,15 @@
+TimeAC0 <- function(x)
+{
+  # Asymtotically unbiased estimator for autocorrelation
+  
+  M <-length(x)
+  r <- array(0, dim = c(M))  # One-sided autocorrelation
+  
+  for (i in 1:M) 
+  {
+    r[i]=(x[1:(M-i+1)]%*%x[i:M])    # Dot product in r
+  }
+  
+  rr=c(rev(r[2:M]),r[1:M]) 
+  return(rr)
+}
